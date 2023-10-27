a6e3be2f1c5607b25412203ef52aa1e160e9327a
YOUSUFABDI <yufis0844@gmail.com>
2023-10-27 05:41:20 +0300

    added some components

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Toaster } from 'react-hot-toast'
 import Header from './components/Header'
 import ThemeSwitch from './components/ThemeSwitch'
 import Home from './components/Home'
@@ -7,6 +8,9 @@ import SectionDivider from './components/SectionDivider'
 import About from './components/About'
 import Projects from './components/Projects'
 import Skills from './components/Skills'
+import Experience from './components/Experience'
+import Contact from './components/Contact'
+import Footer from './components/Footer'
 
 const App: React.FC = () => {
   return (
@@ -15,12 +19,16 @@ const App: React.FC = () => {
       <div className="bg-[#dbd7fb] absolute top-[-1rem] -z-10 left-[-35rem] h-[31.25rem] w-[50rem] rounded-full blur-[10rem] sm:w-[68.75rem] md:left-[-33rem] lg:left-[-28rem] xl:left-[-15rem] 2xl:left-[-5rem] dark:bg-[#676394]"></div>
       <Header />
       <ThemeSwitch />
+      <Toaster />
       <Home>
         <Intro />
         <SectionDivider />
         <About />
         <Projects />
         <Skills />
+        <Experience />
+        <Contact />
+        <Footer />
       </Home>
     </>
   )
---
+++ src/components/Footer.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Footer: React.FC = () => {
+  return (
+    <footer className="mb-10 px-4 text-center text-gray-500">
+      <small className="mb-2 block text-xs">
+        &copy; 2030 Yousuf. All rights reserved.
+      </small>
+      <p className="text-xs">
+        <span className="font-semibold">About this website:</span> built with
+        React & Typscript (Tailwindcss), Framer Motion, Vercel hosting.
+      </p>
+    </footer>
+  )
+}
+
+export default Footer
