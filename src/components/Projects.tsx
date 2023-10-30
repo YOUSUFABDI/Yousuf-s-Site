5e8454398784d757cd8f2cd9f4382e43661e5a32
YOUSUFABDI <yufis0844@gmail.com>
2023-10-30 13:29:49 +0300

    fixed projects component issues

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -8,7 +8,7 @@ const Projects: React.FC = () => {
   const { ref } = useSectionInView('Projects', 0.5)
 
   return (
-    <section ref={ref} className="scroll-mt-28 mb-28">
+    <section id="projects" ref={ref} className="scroll-mt-28 mb-28">
       <SectionHeading>My Projects</SectionHeading>
       <div>
         {projectsData.map((product, index) => (
