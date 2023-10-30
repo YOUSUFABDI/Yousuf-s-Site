1037f19e2acd5b2eb1416bf13abe30cdaadb696a
YOUSUFABDI <yufis0844@gmail.com>
2023-10-30 12:05:53 +0300

    fixed footer date typos

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,7 +4,7 @@ const Footer: React.FC = () => {
   return (
     <footer className="mb-10 px-4 text-center text-gray-500">
       <small className="mb-2 block text-xs">
-        &copy; 2030 Yousuf. All rights reserved.
+        &copy; 2023 Yousuf. All rights reserved.
       </small>
       <p className="text-xs">
         <span className="font-semibold">About this website:</span> built with
