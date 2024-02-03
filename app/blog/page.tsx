df972b20121116d0fa4293ad7c4d0a29ce78b588
YOUSUF <yufis0844@gmail.com>
2024-02-03 11:17:19 +0300

    base url typos

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -1,7 +1,7 @@
 import PostList from "@/app/blog/components/ui/PostList"
 
 export default async function page() {
-  const response = await fetch(`${process.env.BASE_URL}/api/posts`, {
+  const response = await fetch(`https://www.yusufdev.com/api/posts`, {
     method: "GET",
   })
   const posts = await response.json()
