14e35562685defcdeff466069794b3b29077f79c
YOUSUF <yufis0844@gmail.com>
2024-02-04 13:38:39 +0300

    dep error

--- app/page.tsx
+++ app/page.tsx
@@ -10,29 +10,34 @@ export default async function HomePage() {
   const data = await response.data
   const repos = data.repos
 
-  // const posts = await axios.get(`${process.env.BASE_URL}/api/posts`, {
-  //   method: "GET",
-  // })
-  // const postsResponse = (await posts.data) || null
+  const posts = await axios.get(`${process.env.BASE_URL}/api/posts`, {
+    method: "GET",
+  })
+  const postsResponse = (await posts.data) || null
 
   // // get total of posts
-  // const totalOfPost = postsResponse?.length
+  const totalOfPost = postsResponse?.length
 
   // // get total views of all posts
-  // const totalViews: number = postsResponse.reduce(
-  //   (acc: number, post: BlogPostDT) => acc + (post.views || 0),
-  //   0
-  // )
+  const totalViews: number = postsResponse.reduce(
+    (acc: number, post: BlogPostDT) => acc + (post.views || 0),
+    0
+  )
 
   // // get top 3 of the most viewed posts
-  // const topViewedPosts: BlogPostDT[] = postsResponse.sort(
-  //   (a: any, b: any) => (b.views || 0) - (a.views || 0)
-  // )
-  // const mostViewedPosts: BlogPostDT[] = topViewedPosts.slice(0, 3)
+  const topViewedPosts: BlogPostDT[] = postsResponse.sort(
+    (a: any, b: any) => (b.views || 0) - (a.views || 0)
+  )
+  const mostViewedPosts: BlogPostDT[] = topViewedPosts.slice(0, 3)
 
   return (
     <main className="pagePb">
-      <Home repos={repos} totalPost={0} totalViews={0} mostViewedPosts={[]} />
+      <Home
+        repos={repos}
+        totalPost={totalOfPost}
+        totalViews={totalViews}
+        mostViewedPosts={mostViewedPosts}
+      />
     </main>
   )
 }
