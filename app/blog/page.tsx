9c91d60b6d9cfd2bb38543509f1e56177ff4ad01
YOUSUF <yufis0844@gmail.com>
2024-02-04 13:33:14 +0300

    dep err tst

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -2,14 +2,14 @@ import PostList from "@/app/blog/components/ui/PostList"
 import axios from "axios"
 
 export default async function page() {
-  const response = await axios.get(`${process.env.BASE_URL}/api/posts`, {
-    method: "GET",
-  })
-  const posts = (await response.data) || null
+  // const response = await axios.get(`${process.env.BASE_URL}/api/posts`, {
+  //   method: "GET",
+  // })
+  // const posts = (await response.data) || null
 
   return (
     <main className="pagePb">
-      <PostList posts={posts} />
+      <PostList posts={[]} />
     </main>
   )
 }
--- app/page.tsx
+++ app/page.tsx
@@ -3,41 +3,36 @@ import { BlogPostDT } from "@/lib/types"
 import axios from "axios"
 
 export default async function HomePage() {
-  // get total number of github repositories
-  const response = await axios.get(`${process.env.BASE_URL}/api/github`, {
-    method: "GET",
-  })
-  const data = await response.data
-  const repos = data.repos
+  // // get total number of github repositories
+  // const response = await axios.get(`${process.env.BASE_URL}/api/github`, {
+  //   method: "GET",
+  // })
+  // const data = await response.data
+  // const repos = data.repos
 
-  const posts = await axios.get(`${process.env.BASE_URL}/api/posts`, {
-    method: "GET",
-  })
-  const postsResponse = (await posts.data) || null
+  // const posts = await axios.get(`${process.env.BASE_URL}/api/posts`, {
+  //   method: "GET",
+  // })
+  // const postsResponse = (await posts.data) || null
 
-  // get total of posts
-  const totalOfPost = postsResponse?.length
+  // // get total of posts
+  // const totalOfPost = postsResponse?.length
 
-  // get total views of all posts
-  const totalViews: number = postsResponse.reduce(
-    (acc: number, post: BlogPostDT) => acc + (post.views || 0),
-    0
-  )
+  // // get total views of all posts
+  // const totalViews: number = postsResponse.reduce(
+  //   (acc: number, post: BlogPostDT) => acc + (post.views || 0),
+  //   0
+  // )
 
-  // get top 3 of the most viewed posts
-  const topViewedPosts: BlogPostDT[] = postsResponse.sort(
-    (a: any, b: any) => (b.views || 0) - (a.views || 0)
-  )
-  const mostViewedPosts: BlogPostDT[] = topViewedPosts.slice(0, 3)
+  // // get top 3 of the most viewed posts
+  // const topViewedPosts: BlogPostDT[] = postsResponse.sort(
+  //   (a: any, b: any) => (b.views || 0) - (a.views || 0)
+  // )
+  // const mostViewedPosts: BlogPostDT[] = topViewedPosts.slice(0, 3)
 
   return (
     <main className="pagePb">
-      <Home
-        repos={repos}
-        totalPost={totalOfPost}
-        totalViews={totalViews}
-        mostViewedPosts={mostViewedPosts}
-      />
+      <Home repos={0} totalPost={0} totalViews={0} mostViewedPosts={[]} />
     </main>
   )
 }
