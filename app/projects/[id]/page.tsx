19d82c195250554c541a484d3d3fb4504c4eb24d
YOUSUF <yufis0844@gmail.com>
2024-02-10 20:50:40 +0300

    cleaned and organized project page code

--- app/projects/[id]/page.tsx
+++ app/projects/[id]/page.tsx
@@ -7,25 +7,25 @@ import Contact from "@/layouts/Contact"
 import axios from "axios"
 
 export default async function page({ params }: ProjectPagePropsDT) {
-  // const response = await axios.get(`${process.env.BASE_URL}/api/projects`, {
-  //   method: "GET",
-  //   headers: { "Cache-Control": "no-cache" },
-  // })
-  // const projects = await response.data
-  // const project =
-  //   (await projects.find((project: any) => project.id == params.id)) || null
+  const response = await axios.get(`${process.env.BASE_URL}/api/projects`, {
+    method: "GET",
+    headers: { "Cache-Control": "no-cache" },
+  })
+  const projects = await response.data
+  const project =
+    (await projects.find((project: any) => project.id == params.id)) || null
 
   return (
     <main className="flex flex-col gap-10 my-14 pagePb text-lightSecondary dark:text-darkSecondary">
       <ProjectHeader
-        createdDT={33}
-        projectName={"name"}
-        describtion={"description"}
-        githubLink={"githubLink"}
-        liveDemo={"liveDemo"}
+        createdDT={project.createdDT}
+        projectName={project.projectName}
+        describtion={project.description}
+        githubLink={project.githubLink}
+        liveDemo={project.liveDemo}
       />
-      <ProjectDemo images={[]} />
-      <ProjectTags tags={[]} />
+      <ProjectDemo images={project.images} />
+      <ProjectTags tags={project.tags} />
       <Contact />
       <GoBack link="projects" text="All projects" />
     </main>
