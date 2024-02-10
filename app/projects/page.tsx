74c91a907e0749b02aeee4ebf0395c7197acadbf
YOUSUF <yufis0844@gmail.com>
2024-02-10 20:45:58 +0300

    cleaned and organized project page code

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -2,15 +2,15 @@ import Projects from "@/components/Projects/Projects"
 import axios from "axios"
 
 export default async function ProjectsPage() {
-  const response = await axios.get(`${process.env.BASE_URL}/api/projects`, {
-    method: "GET",
-    headers: { "Cache-Control": "no-cache" },
-  })
-  const projects = (await response.data) || null
+  // const response = await axios.get(`${process.env.BASE_URL}/api/projects`, {
+  //   method: "GET",
+  //   headers: { "Cache-Control": "no-cache" },
+  // })
+  // const projects = (await response.data) || null
 
   return (
     <main className="pagePb">
-      <Projects projects={projects} />
+      <Projects projects={[]} />
     </main>
   )
 }
