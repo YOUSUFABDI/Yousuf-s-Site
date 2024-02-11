2344093be6fe7a5f34d324c7a73e018efe865f67
YOUSUF <yufis0844@gmail.com>
2024-02-11 10:35:23 +0300

    added skelton fallback to blog p

--- app/blog/[slug]/page.tsx
+++ app/blog/[slug]/page.tsx
@@ -1,3 +1,5 @@
+"use client"
+
 import React from "react"
 import BlogInro from "../components/ui/BlogInro"
 import SubTitle from "@/layouts/SubTitle"
@@ -9,35 +11,50 @@ import Article from "../components/ui/Article"
 import Note from "../../../layouts/Note"
 import GoBack from "../../../layouts/GoBack"
 import { BlogPostDT, PostPagePropsDT } from "@/lib/types"
+import Skeleton from "../components/ui/Skeleton"
+import useSWR from "swr"
+import Fetcher from "@/lib/fetcher"
 
-export default async function page({ params }: PostPagePropsDT) {
-  const response = await fetch(
-    `${process.env.BASE_URL}/api/posts/?slug=${params.slug}`,
-    { method: "GET", cache: "no-cache" }
+export default function page({ params }: PostPagePropsDT) {
+  const { data, isLoading } = useSWR(
+    `/api/posts/?slug=${params.slug}`,
+    Fetcher,
+    {
+      revalidateOnFocus: false,
+    }
   )
-  const post: BlogPostDT = (await response.json()) || null
-  console.log(post.views)
+  const post: BlogPostDT = data
+
+  if (isLoading) {
+    return (
+      <Article>
+        {[1, 2, 3, 4, 5].map((el, index) => (
+          <Skeleton key={`${el + index}`} />
+        ))}
+      </Article>
+    )
+  }
 
   return (
     <Article>
       <Box>
-        <BlogTitle>{post.mainTitle}</BlogTitle>
-        <SubTitle>{post.description}</SubTitle>
+        <BlogTitle>{post?.mainTitle}</BlogTitle>
+        <SubTitle>{post?.description}</SubTitle>
         <BlogInro
-          name={post.creatorName}
-          img={post.creatorImage}
-          date={post.createdDate}
-          views={post.views}
+          name={post?.creatorName}
+          img={post?.creatorImage}
+          date={post?.createdDate}
+          views={post?.views}
         />
         <Avatar
-          img={post.allImages[0].image}
+          img={post?.allImages[0].image}
           customClasses="w-[1020px] rounded-md"
           width={1020}
           height={548}
         />
       </Box>
 
-      {post.titles.map((title) => (
+      {post?.titles.map((title) => (
         <Box key={title.titleID}>
           <BlogTitle>{title.title}</BlogTitle>
           {title.paragraphs.map((paragraph) => (
@@ -61,9 +78,9 @@ export default async function page({ params }: PostPagePropsDT) {
         </Box>
       ))}
 
-      <div className={`${post.allImages[1] ? "block" : "hidden"}`}>
+      <div className={`${post?.allImages[1] ? "block" : "hidden"}`}>
         <Avatar
-          img={post.allImages[1] && post.allImages[1].image}
+          img={post?.allImages[1] && post?.allImages[1].image}
           customClasses="w-[1020px] rounded-md"
           width={1020}
           height={548}
---
+++ app/blog/components/ui/Skeleton.tsx
@@ -0,0 +1,9 @@
+const Skeleton = () => (
+  <div className="bg-gray-200 rounded-md p-4 animate-pulse">
+    <div className="h-6 w-4/5 bg-gray-300 mb-4"></div>
+    <div className="h-4 w-3/5 bg-gray-300 mb-2"></div>
+    <div className="h-4 w-2/5 bg-gray-300"></div>
+  </div>
+)
+
+export default Skeleton
