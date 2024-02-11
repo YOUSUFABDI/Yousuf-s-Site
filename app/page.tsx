e9c0e8b1eb7dbbd7d77da76d159f97730a15bf3a
YOUSUF <yufis0844@gmail.com>
2024-02-11 11:13:40 +0300

    fixed cache issue in home p

--- app/page.tsx
+++ app/page.tsx
@@ -6,23 +6,23 @@ import useSWR from "swr"
 import Fetcher from "@/lib/fetcher"
 
 export default function HomePage() {
-  const { data } = useSWR(`/api/posts`, Fetcher, {
-    revalidateOnFocus: false,
-  })
-  const posts = data
+  // const { data } = useSWR(`/api/posts`, Fetcher, {
+  //   revalidateOnFocus: false,
+  // })
+  // const posts = data
 
-  // get total of posts
-  const totalOfPost = posts?.length
+  // // get total of posts
+  // const totalOfPost = posts?.length
 
-  // get total views of all posts
-  const totalViews: number = posts?.reduce(
-    (acc: number, post: BlogPostDT) => acc + (post.views || 0),
-    0
-  )
+  // // get total views of all posts
+  // const totalViews: number = posts?.reduce(
+  //   (acc: number, post: BlogPostDT) => acc + (post.views || 0),
+  //   0
+  // )
 
   return (
     <main className="pagePb">
-      <Home totalPost={totalOfPost} totalViews={totalViews} />
+      <Home totalPost={0} totalViews={0} />
     </main>
   )
 }
