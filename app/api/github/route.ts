ae405d8e68c03db6daab984731adb58f6fb4adcd
YOUSUF <yufis0844@gmail.com>
2024-02-17 11:36:36 +0300

    github repo api error

--- app/api/github/route.ts
+++ app/api/github/route.ts
@@ -1,13 +1,8 @@
 import { NextRequest } from "next/server"
 
 export async function GET(req: NextRequest) {
-  const username = "YOUSUFABDI"
-  if (!username) {
-    return new Response("Please provide a username", { status: 200 })
-  }
-
   try {
-    const url = `https://api.github.com/users/${username}/repos?per_page=100`
+    const url = "https://api.github.com/users/YOUSUFABDI/repos?per_page=100"
     const response = await fetch(url)
     const data = await response.json()
     const repos = data.length
